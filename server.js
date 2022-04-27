4b18b732748e76b027b07c178901ab8c3d3b1332
Mohammad Rahman <mbrahman@gmail.com>
2022-04-27 09:03:35 -0400

    created mongodb connection

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.set('debug', true)
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
